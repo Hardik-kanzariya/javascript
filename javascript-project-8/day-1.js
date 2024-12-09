51d2b22f7c1ca9b0b9c5c5bd57fa24f738920433
Hardik kanzariya <hardikkanzariya620@gmail.com>
2024-12-09 18:32:28 +0530

    project-8-day-1-2-Add-view product-5-12-24

--- javascript-project-8/day-1.js
+++ javascript-project-8/day-1.js
@@ -1,22 +1,60 @@
-let Productname = document.getElementById('pname');
-let description = document.getElementById('pdesc');
-let Category = document.getElementById('pcat');
-let Price = document.getElementById('pprice');
+let pname = document.getElementById('pname');
+let pdesc = document.getElementById('pdesc');
+let pcat = document.getElementById('pcat');
+let pprice = document.getElementById('pprice');
+
+const tbody = document.querySelector("#card-container");
+
 const button = document.getElementById('btn');
 
 
 button.addEventListener('click',() =>{
-    let data = JSON.parse(localStorage.getItem('users')) || [];
+    let data = JSON.parse(localStorage.getItem('product')) || [];
+    console.log(pname.value);
     data.push(
         {
-            Productname: pname.value,
-            dedescription: pdesc.value,
-            Category: pcat.value,
-            Price:pprice.value
+            productName: pname.value,
+            description: pdesc.value,
+            category: pcat.value,
+            price:pprice.value
 
         }
     )
     
-    localStorage.setItem('users',JSON.stringify(data))
+    localStorage.setItem('product',JSON.stringify(data));
+    loadData();
+    pname.value=""
+    pdesc.value=""
+    pcat.value=""
 
 });
+
+const loadData = () =>{
+
+  const cardContainer = document.getElementById('card-container');
+cardContainer.innerHTML = "";
+    
+    const product = JSON.parse(localStorage.getItem('product')) || [];
+    let result = "";
+product.forEach((pro) =>{
+    let row = `
+    <div id="card-container" class="d-flex flex-wrap justify-content-center">
+          <div class="card m-3" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">ProductName:- ${pro.productName}</h5>
+              <p class="card-text"><strong>Description:-${pro.description}</strong><br/>
+                <strong>Category:-${pro.category}</strong><br/>
+                <strong>Price:-${pro.price}</strong><br/>
+                
+              </p>
+              <a href="#" class="btn btn-primary">Add To Cart</a>
+            </div>
+          </div>
+        </div>
+                `
+    result = result + row; 
+})
+        tbody.innerHTML = result
+
+};
+loadData()
